23749716bc1322a8e4ef3b98506a14279525fc3c
Cesar Solano <171cas@gmail.com>
2022-09-30 16:16:27 -0500

    form

---
+++ app/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import (BooleanField, DateField, StringField,SubmitField,TextAreaField,TimeField)
+from wtforms.widgets import DateInput, TimeInput
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime, timedelta
+
+
+def next_block(delta=0):
+    def time():
+        now = datetime.now()
+        return now - timedelta(minutes=now.minute % 15 - delta - 15,
+                               seconds=now.second)
+    return time
+
+
+di = {'default': datetime.now, 'widget': DateInput()}
+sti = {'default': next_block(), 'widget': TimeInput()}
+eti = {'default': next_block(60), 'widget': TimeInput()}
+
+
+class AppointmentForm(FlaskForm):
+    name = StringField("Name", DataRequired)
+    start_date = DateField("Start Date", DataRequired, **di)
+    start_time = DateField("Start Time", DataRequired, **sti)
+    end_date = DateField("End Date", DataRequired, **di)
+    end_time = DateField("End Time", DataRequired, **sti)
+    description = TextAreaField("description", DataRequired)
+    private = BooleanField("Private?", DataRequired)
+    submit = SubmitField("Create an appointment")
+
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+        if start >= end:
+            msg = "End date/time must come after start date/time"
+            raise ValidationError(msg)
+        if form.start_date.data != form.end_date.data:
+            msg = "End date must be the same as start date"
+            raise ValidationError(msg)
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,24 @@
 from flask import Blueprint, render_template
 import os
 import sqlite3
+from .forms import AppointmentForm
 
 bp = Blueprint('main', __name__, url_prefix="/")
 DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route("/")
 def main():
+    form = AppointmentForm()
+    # if form.validate_on_submit():
+    #     print('\n\n\n\n', form, '\n\n\n\n\n')
+        # with sqlite3.connect(DB_FILE) as conn:
+        #     curs = conn.cursor()
+        #     curs.execute('SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;')
+        #     appointments = curs.fetchall()
+
     with sqlite3.connect(DB_FILE) as conn:
         curs = conn.cursor()
         curs.execute('SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;')
         appointments = curs.fetchall()
-    print('\n\n\n\n',appointments,'\n\n\n\n\n')
-    return render_template('main.html', rows=appointments)
+    # print('\n\n\n\n',appointments,'\n\n\n\n\n')
+    return render_template('main.html', rows=appointments, form=form)
